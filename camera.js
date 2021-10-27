8b548fc1f7cc47f576dc604c264e93784c57dd0b
armblog <admin@armblog.net>
2021-10-27 20:23:20 +0400

    Update camera.js

--- camera.js
+++ camera.js
@@ -1097,7 +1097,7 @@
 		if(!$('.imgLoaded',slide).length){
 			var imgUrl = allImg[slideI];
 			var imgLoaded = new Image();
-			imgLoaded.src = imgUrl;
+			//imgLoaded.src = imgUrl;
 			slide.css('visibility','hidden');
 			slide.prepend($(imgLoaded).attr('class','imgLoaded').css('visibility','hidden'));
 			var wT, hT;
